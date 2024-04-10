b186e3c0d6e7c652bd9db9df45b4afedf735344b
Michael Gingras <mcg79@cornell.edu>
2024-04-10 17:05:34 -0600

    new UI for creation page

--- packages/app/src/app/idea/new/page.tsx
+++ packages/app/src/app/idea/new/page.tsx
@@ -58,24 +58,74 @@ const NewIdeaPage = () => {
   };
 
   return (
-    <section className="mt-24 w-[800px] mx-auto">
-      <h1 className="text-2xl text-neutral-700 mb-2">New idea</h1>
-      <form className="border p-4 space-y-4" onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex flex-col">
-          <label>Title</label>
-          <input type="text" className="border p-2" {...register("title")} />
+    <section className="mt-24 w-[1200px] mx-auto bg-white py-12 rounded-2xl border-2 border-[#F0F2F5]">
+      <form
+        className="bg-white w-[800px] mx-auto"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="space-y-12">
+          <div className="pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              New idea
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              Submit a new idea to the wave.
+            </p>
+
+            <div className="sm:col-span-3 mt-10">
+              <label
+                htmlFor="title"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Title
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  id="title"
+                  {...register("title")}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-neutral-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="col-span-full">
+                <label
+                  htmlFor="description"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Description
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    id="description"
+                    rows={3}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-neutral-600 sm:text-sm sm:leading-6"
+                    defaultValue={""}
+                    {...register("description")}
+                  />
+                </div>
+                <p className="mt-2 text-sm leading-6 text-gray-400">
+                  The details of your idea. This will be submitted as the body
+                  of the proposal.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col">
-          <label>Description</label>
-          <textarea className="border p-2" {...register("description")} />
+
+        <div className="mt-6 flex items-center justify-end">
+          <button
+            type="submit"
+            disabled={isPending}
+            className="px-4 py-2 rounded-full bg-neutral-800 hover:bg-neutral-700 transition-colors text-white"
+          >
+            Save
+          </button>
+          {isConfirming && <div>Waiting for confirmation...</div>}
+          {isConfirmed && <div>Transaction confirmed.</div>}
         </div>
-        <div className="flex flex-col">
-          <label>Amount</label>
-          <input type="number" className="border p-2" {...register("amount")} />
-        </div>
-        <button disabled={isPending}>Submit</button>
-        {isConfirming && <div>Waiting for confirmation...</div>}
-        {isConfirmed && <div>Transaction confirmed.</div>}
       </form>
     </section>
   );
