dcb6d96db8676a2c6f56f94f04ec7961e3d4794c
Michael Gingras <mcg79@cornell.edu>
2024-06-01 12:35:24 -0600

    Fixes typescript errors

--- packages/app/src/components/IdeaCard/ActionDisplay.tsx
+++ packages/app/src/components/IdeaCard/ActionDisplay.tsx
@@ -4,7 +4,7 @@ const ActionDisplay = ({ actions }: { actions: string }) => {
   const parsedActions = JSON.parse(actions);
   return (
     <pre className="bg-neutral-100 p-2 mt-2 rounded">
-      {parsedActions.targets.map((_target, index) => {
+      {parsedActions.targets.map((_target: any, index: any) => {
         return (
           <div key={index} className="flex flex-row space-x-2">
             <span className="text-xs px-2 py-1 bg-neutral-100 text-neutral-400 rounded-full self-start uppercase">
