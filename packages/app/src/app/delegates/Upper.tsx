1bdd93c5161e2202e8e0dc10cc8b427ef9ca4821
Michael Gingras <mcg79@cornell.edu>
2024-06-02 19:01:40 -0600

    Add undelegate modal

--- packages/app/src/app/delegates/Upper.tsx
+++ packages/app/src/app/delegates/Upper.tsx
@@ -3,15 +3,30 @@
 import { useState } from "react";
 import Button from "@/components/ui/Button";
 import CreateDelegateProxyForm from "@/components/CreateDelegateProxyForm";
+import UndelegateProxyForm from "@/components/UndelegateProxyForm";
 import Modal from "@/components/ui/Modal";
 import UserStats from "./UserStats";
 
 const Upper = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDelegateModalOpen, setIsDelegateModalOpen] = useState(false);
+  const [isUnDelegateModalOpen, setIsUnDelegateModalOpen] = useState(false);
   return (
     <>
-      <Modal isOpen={isModalOpen} setIsOpen={() => setIsModalOpen(false)}>
-        <CreateDelegateProxyForm closeModal={() => setIsModalOpen(false)} />
+      <Modal
+        isOpen={isDelegateModalOpen}
+        setIsOpen={() => setIsDelegateModalOpen(false)}
+      >
+        <CreateDelegateProxyForm
+          closeModal={() => setIsDelegateModalOpen(false)}
+        />
+      </Modal>
+      <Modal
+        isOpen={isUnDelegateModalOpen}
+        setIsOpen={() => setIsUnDelegateModalOpen(false)}
+      >
+        <UndelegateProxyForm
+          closeModal={() => setIsUnDelegateModalOpen(false)}
+        />
       </Modal>
       <section className="w-[600px] mx-auto pt-12 pb-8">
         <div className="flex flex-row justify-between items-center">
@@ -19,13 +34,23 @@ const Upper = () => {
             Delegation
           </h1>
 
-          <Button
-            type="primary"
-            title="Add voting power"
-            onClick={() => {
-              setIsModalOpen(true);
-            }}
-          />
+          <div className="flex flex-row space-x-2">
+            <Button
+              type="secondary"
+              title="Undelegate"
+              onClick={() => {
+                setIsUnDelegateModalOpen(true);
+              }}
+            />
+
+            <Button
+              type="primary"
+              title="Add voting power"
+              onClick={() => {
+                setIsDelegateModalOpen(true);
+              }}
+            />
+          </div>
         </div>
         <UserStats />
       </section>
---
+++ packages/app/src/components/UndelegateProxyForm.tsx
@@ -0,0 +1,121 @@
+import {
+  useReadContract,
+  useAccount,
+  useWriteContract,
+  useWaitForTransactionReceipt,
+} from "wagmi";
+import { configAddresses } from "@/lib/constants";
+import { NounsTokenABI } from "@/abi/NounsToken";
+import Button from "./ui/Button";
+
+/**
+ * TODO:
+ * Need to figure out a better flow for delegating -> registering -> keeping track of delegates
+ * What happens when someone undelegates but is still registered? That should be okay.
+ * What happens when someone transfers a noun?
+ * How do you unregister?
+ */
+const UndelegateProxyForm = ({ closeModal }: { closeModal: () => void }) => {
+  const { address } = useAccount();
+
+  const { data: nounsBalance } = useReadContract({
+    address: configAddresses.NounsTokenHarness as `0x${string}`,
+    abi: NounsTokenABI,
+    functionName: "balanceOf",
+    args: [address as `0x${string}`],
+  });
+
+  const { data: delegatedTo, refetch: refetchDelegateTo } = useReadContract({
+    address: configAddresses.NounsTokenHarness as `0x${string}`,
+    abi: NounsTokenABI,
+    functionName: "delegates",
+    args: [address as `0x${string}`],
+  });
+
+  const {
+    data: hash,
+    writeContractAsync: delegateTo,
+    isPending: isDelegateWritePending,
+  } = useWriteContract();
+
+  console.log("hash", hash);
+  const { isFetching: isDelegateWriteFetching } = useWaitForTransactionReceipt({
+    hash,
+  });
+
+  const undelegateHelper = async () => {
+    await delegateTo(
+      {
+        chainId: 84532,
+        address: configAddresses.NounsTokenHarness as `0x${string}`,
+        abi: NounsTokenABI,
+        functionName: "delegate",
+        args: [address as `0x${string}`],
+      },
+      {
+        onSuccess: () => {
+          console.log("we are refetching baby...");
+          refetchDelegateTo();
+        },
+      }
+    );
+  };
+
+  return (
+    <div>
+      <h2 className="font-bold text-center">Undelegate voting power</h2>
+      <p className="text-neutral-700 text-xs text-center mt-2">
+        By undelegating you will remove your voting power from the delegate
+        below, and will transfer the voting power back to your account.
+      </p>
+
+      <div className="border rounded mt-6">
+        <div className="p-4 border-b">
+          <label
+            htmlFor="type"
+            className="block text-sm font-medium leading-6 text-neutral-700"
+          >
+            Voting power
+          </label>
+          <div className="mt-1">
+            <span className="p-2 rounded w-full bg-neutral-100 block text-sm text-neutral-500">
+              {nounsBalance?.toString()}
+            </span>
+          </div>
+        </div>
+
+        <div className="p-4">
+          <label
+            htmlFor="type"
+            className="block text-sm font-medium leading-6 text-neutral-700"
+          >
+            Current delegate
+          </label>
+          <div className="mt-1">
+            <span className="p-2 rounded w-full bg-neutral-100 block text-sm text-neutral-500">
+              {delegatedTo}
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-8 flex justify-end space-x-2">
+        <Button onClick={closeModal} type="secondary" title={"Cancel"} />
+
+        <Button
+          type="primary"
+          title={
+            isDelegateWritePending || isDelegateWriteFetching
+              ? "Pending..."
+              : "Undelegate"
+          }
+          onClick={() => {
+            undelegateHelper();
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UndelegateProxyForm;
