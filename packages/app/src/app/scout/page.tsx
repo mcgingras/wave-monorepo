6c669dacef9a2e3e402b41fa391bdf44a000dc1b
Michael Gingras <mcg79@cornell.edu>
2024-06-29 16:47:08 -0600

    sdsd

---
+++ packages/app/src/app/scout/page.tsx
@@ -0,0 +1,13 @@
+const ScoutsPage = () => {
+  return (
+    <div className="min-h-[calc(100vh-165px)] mt-[65px] pt-12 flex flex-col bg-neutral-100">
+      <section className="container mx-auto pb-12">
+        <h2 className="polymath-disp font-bold text-2xl text-neutral-800 pt-6">
+          Supporters
+        </h2>
+      </section>
+    </div>
+  );
+};
+
+export default ScoutsPage;
