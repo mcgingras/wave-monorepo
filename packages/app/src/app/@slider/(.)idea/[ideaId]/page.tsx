3540fffc7598ff011ac06284f6dc5e96a839cad1
Michael Gingras <mcg79@cornell.edu>
2024-07-01 16:07:04 -0600

    UseClickAway

--- packages/app/src/app/@slider/(.)idea/[ideaId]/page.tsx
+++ packages/app/src/app/@slider/(.)idea/[ideaId]/page.tsx
@@ -1,18 +1,70 @@
 import Drawer from "@/app/components/Drawer";
+import { IdeaToken } from "@/models/IdeaToken/types";
 
-const Page = () => {
+const getIdea = async (id: bigint) => {
+  const url = process.env.NEXT_PUBLIC_GRAPHQL_URL!;
+  const query = `
+      query GetIdeaToken($ideaTokenId: BigInt!) {
+          ideaToken(id: $ideaTokenId) {
+              id
+              author
+              title
+              description
+              createdAt
+              actions
+              isArchived
+              supporters {
+                  items {
+                  reason
+                  balance
+                  owner
+                  }
+              }
+          }
+          }
+     `;
+
+  const graphqlRequest = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables: { ideaTokenId: id },
+    }),
+  };
+
+  try {
+    const data = await fetch(url, graphqlRequest);
+    const json = await data.json();
+    return json.data.ideaToken;
+  } catch (e) {
+    console.log("error", e);
+    return null;
+  }
+};
+
+const Page = async ({ params }: { params: { ideaId: bigint } }) => {
+  const { ideaId } = params;
+  const ideaToken = (await getIdea(ideaId)) as IdeaToken;
+
   return (
-    <Drawer title="fa">
-      <section className="px-4 sm:px-6">
+    <Drawer ideaToken={ideaToken}>
+      <section className="px-4">
         <h1 className="text-2xl font-semibold tracking-wide text-gray-900">
-          Idea
+          {ideaToken.title}
         </h1>
-        <p className="mt-2 text-sm text-gray-500">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam
-          velit, vestibulum in vel, lobortis pulvinar. Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit. Nulla quam velit, vestibulum in
-          vel, lobortis pulvinar.
-        </p>
+        <div className="border-b mt-4">
+          <h3 className="text-sm text-neutral-500 font-bold">2 actions</h3>
+        </div>
+        <div className="border-b mt-4">
+          <h3 className="text-sm text-neutral-500 font-bold">Description</h3>
+        </div>
+        <p className="mt-2 text-sm text-gray-500">{ideaToken.description}</p>
+        <div className="border-b mt-4">
+          <h3 className="text-sm text-neutral-500 font-bold">12 supporters</h3>
+        </div>
       </section>
     </Drawer>
   );
--- packages/app/src/app/components/Drawer.tsx
+++ packages/app/src/app/components/Drawer.tsx
@@ -1,17 +1,50 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useRef, useLayoutEffect, useEffect } from "react";
 import { Dialog } from "@headlessui/react";
 import { useRouter } from "next/navigation";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { AnimatePresence, motion } from "framer-motion";
+import { IdeaToken } from "@/models/IdeaToken/types";
+import Button from "@/components/ui/Button";
+import { ArrowUpIcon } from "@heroicons/react/24/solid";
+import { ArrowDownIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+export function useClickAway(cb: any) {
+  const ref = useRef(null);
+  const refCb = useRef(cb);
+
+  useLayoutEffect(() => {
+    refCb.current = cb;
+  });
+
+  useEffect(() => {
+    const handler = (e: any) => {
+      const element = ref.current as any;
+      if (element && !element.contains(e.target)) {
+        refCb.current(e);
+      }
+    };
+
+    document.addEventListener("mousedown", handler);
+    document.addEventListener("touchstart", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+      document.removeEventListener("touchstart", handler);
+    };
+  }, []);
+
+  return ref;
+}
 
 const Drawer = ({
+  ideaToken,
   children,
-  title,
 }: {
+  ideaToken: IdeaToken;
   children: React.ReactElement;
-  title: string;
 }) => {
   const [open, setOpen] = useState(true);
 
@@ -23,6 +56,10 @@ const Drawer = ({
       router.back();
     }, 300);
   };
+
+  const ref = useClickAway(() => {
+    onClose();
+  });
 
   return (
     <AnimatePresence>
@@ -44,24 +81,55 @@ const Drawer = ({
                   animate={{ translateX: "0%" }}
                   exit={{ translateX: "100%" }}
                   transition={{ type: "spring", stiffness: 300, damping: 30 }}
+                  ref={ref}
                 >
-                  <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl rounded-lg">
-                    <div className="px-4 sm:px-6 pb-4 sm:pb-6 border-b">
+                  <div className="flex h-full flex-col overflow-y-scroll bg-white py-4 shadow-xl rounded-lg">
+                    <div className="px-4 pb-4 border-b">
                       <div className="flex items-start justify-between">
-                        <div className="ml-3 flex h-7 items-center">
-                          <button
-                            type="button"
-                            className="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                            onClick={onClose}
-                          >
-                            <span className="absolute -inset-2.5" />
-                            <span className="sr-only">Close panel</span>
-                            <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                          </button>
+                        <div className="flex flex-row space-x-2 items-center">
+                          <div className="flex h-7 items-center">
+                            <button
+                              type="button"
+                              className="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                              onClick={onClose}
+                            >
+                              <span className="sr-only">Close panel</span>
+                              <XMarkIcon
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              />
+                            </button>
+                          </div>
+                          <Button
+                            type="primary"
+                            onClick={() => {
+                              window.location.href = `/idea/${ideaToken.id}`;
+                            }}
+                            title="Idea page"
+                          />
+
+                          <Button
+                            type="secondary"
+                            title="Support"
+                            onClick={() => {
+                              // pass
+                            }}
+                          />
                         </div>
-                        <h2 className="text-base font-semibold leading-6 text-gray-900">
-                          {title}
-                        </h2>
+                        <div className="flex flex-row space-x-1">
+                          <Link
+                            href={`/idea/${Number(ideaToken.id) - 1}`}
+                            className="rounded-l-lg bg-neutral-100 hover:bg-neutral-200 cursor-pointer transition-colors p-2"
+                          >
+                            <ArrowUpIcon className="text-neutral-500 h-5 w-5" />
+                          </Link>
+                          <Link
+                            href={`/idea/${Number(ideaToken.id) + 1}`}
+                            className="rounded-r-lg bg-neutral-100 hover:bg-neutral-200 cursor-pointer transition-colors p-2"
+                          >
+                            <ArrowDownIcon className="text-neutral-500 h-5 w-5" />
+                          </Link>
+                        </div>
                       </div>
                     </div>
                     <div className="relative mt-6 flex-1">{children}</div>
