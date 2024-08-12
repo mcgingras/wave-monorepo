b91e28529fb0003ee8ecd2eeeb271e91525f351b
Michael Gingras <mcg79@cornell.edu>
2024-08-12 17:10:09 -0600

    export config addresses

--- packages/ponder/ponder.config.ts
+++ packages/ponder/ponder.config.ts
@@ -7,7 +7,7 @@ import { NounsTokenABI } from "./abi/NounsToken";
 
 const mainnetStartBlock = 20466000;
 
-const configAddresses = {
+export const configAddresses = {
   Wave: "0x00000000008DDB753b2dfD31e7127f4094CE5630",
   IdeaTokenHub: "0x000000000088b111eA8679dD42f7D55512fD6bE8",
   NounsTokenHarness: "0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03",
