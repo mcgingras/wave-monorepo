6a0ffa7a838d58dcd816f62533d33e33cdad202a
Michael Gingras <mcg79@cornell.edu>
2024-08-12 17:06:14 -0600

    Moves to mainnet only

--- packages/ponder/ponder.config.ts
+++ packages/ponder/ponder.config.ts
@@ -5,39 +5,16 @@ import { DelegateABI } from "./abi/Delegate";
 import { IdeaTokenHubABI } from "./abi/IdeaTokenHub";
 import { NounsTokenABI } from "./abi/NounsToken";
 
-const isProd = true;
-const baseSepoliaStartBlock = 12000000;
 const mainnetStartBlock = 20466000;
 
-const baseSepoliaContracts = {
-  Wave: "0x443f1F80fBB72Fa40cA70A93a0139852b0563961",
-  IdeaTokenHub: "0xAFFED3815a60aACeACDA3aE53425f053eD6Efc4d",
-  NounsTokenHarness: "0xE8b46D16107e1d562B62B5aA8d4bF9A60e6c51b4",
-};
-
-const mainnetContracts = {
+const configAddresses = {
   Wave: "0x00000000008DDB753b2dfD31e7127f4094CE5630",
   IdeaTokenHub: "0x000000000088b111eA8679dD42f7D55512fD6bE8",
   NounsTokenHarness: "0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03",
 };
 
-export const configAddresses = {
-  Wave: isProd ? mainnetContracts.Wave : baseSepoliaContracts.Wave,
-  IdeaTokenHub: isProd
-    ? mainnetContracts.IdeaTokenHub
-    : baseSepoliaContracts.IdeaTokenHub,
-  NounsTokenHarness: isProd
-    ? mainnetContracts.NounsTokenHarness
-    : baseSepoliaContracts.NounsTokenHarness,
-};
-
 export default createConfig({
   networks: {
-    baseSepolia: {
-      chainId: 84532,
-      // @ts-ignore
-      transport: http(process.env.PONDER_RPC_URL_84532!),
-    },
     mainnet: {
       chainId: 1,
       // @ts-ignore
@@ -46,19 +23,19 @@ export default createConfig({
   },
   contracts: {
     Wave: {
-      network: isProd ? "mainnet" : "baseSepolia",
+      network: "mainnet",
       abi: WaveHarnessABI,
       address: configAddresses.Wave as `0x${string}`,
-      startBlock: isProd ? mainnetStartBlock : baseSepoliaStartBlock,
+      startBlock: mainnetStartBlock,
     },
     IdeaTokenHub: {
-      network: isProd ? "mainnet" : "baseSepolia",
+      network: "mainnet",
       abi: IdeaTokenHubABI,
       address: configAddresses.IdeaTokenHub as `0x${string}`,
-      startBlock: isProd ? mainnetStartBlock : baseSepoliaStartBlock,
+      startBlock: mainnetStartBlock,
     },
     Delegate: {
-      network: isProd ? "mainnet" : "baseSepolia",
+      network: "mainnet",
       abi: DelegateABI,
       factory: {
         address: configAddresses.Wave as `0x${string}`,
@@ -67,13 +44,13 @@ export default createConfig({
         ),
         parameter: "delegate",
       },
-      startBlock: isProd ? mainnetStartBlock : baseSepoliaStartBlock,
+      startBlock: mainnetStartBlock,
     },
     NounsToken: {
-      network: isProd ? "mainnet" : "baseSepolia",
+      network: "mainnet",
       abi: NounsTokenABI,
       address: configAddresses.NounsTokenHarness as `0x${string}`,
-      startBlock: isProd ? mainnetStartBlock : baseSepoliaStartBlock,
+      startBlock: mainnetStartBlock,
     },
   },
 });
